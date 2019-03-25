892d3b4b3f219e92ce21caecaa9caf5d3692e071
reactive-jest-9572 <ryan12-12@hotmail.com>
2019-03-25 17:04:28 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,2 @@
 # Define the variable board below.
-board = Row[1] + Row[2] + Row[3]
-
-Row[1] = [" " , " " , " "]
-
-Row[2] = [" " , " " , " "]
-
-Row[3] = [" " , " " , " "]
+board = [" " , " " , " " , " " , " " , " " , " " , " " , " "]
